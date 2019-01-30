ec002f39fb973616ddb389db90a9289c92bcd08b
Dinesh Bhattarai <dbhattarai252@gmail.com>
2019-01-30 09:36:11 +0545

    update run env

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const httpProxy = require('http-proxy')
 const path = require('path')
 
 const apiServer = 'http://localhost:8080'
+const developmentReactServer = 'http://localhost:3000'
 
 const app = express()
 const apiProxy = httpProxy.createProxyServer()
@@ -17,10 +18,16 @@ app.all("/api/*", (req, res) => {
       apiProxy.web(req, res, { target: apiServer})
 })
 
+// Remove following code block in production.
+app.all('*', (req, res) => {
+      apiProxy.web(req, res, { target: developmentReactServer})
+
+})
+
 app.use(function (req, res, next) {
   res.status(200).sendFile(path.join(__dirname, publicFolder, 'index.html'))
 })
 
-app.listen(3000, ()=>{
-  console.log("http://localhost:3000/")
+app.listen(3001, ()=>{
+  console.log("http://localhost:3001/")
 })
